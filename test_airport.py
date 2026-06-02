9d2b96308d5ef1ab330108be2d71e9dfe199b523
claudiahernandezmartin-max <claudia.hernandez.martin@estudiantat.upc.edu>
2026-06-02 10:11:59 +0200

    Comentaris

--- test_airport.py
+++ test_airport.py
@@ -1,7 +1,11 @@
 from airport import *
+#Creem un aeroport amb el codi de Barcelona i les seves coordenades
 airport = Airport ("LEBL", 41.297445, 2.0832941 )
+#Mirem si aquest aeroport forma part de l'espai Schengen
 SetSchengen(airport)
+#Mostrem per pantalla la informació de l'aeroport
 PrintAirport (airport)
+
 """ Test Pas 3
 airports = LoadAirports("Airports.txt")
 print(f"Aeroports carregats: {len(airports)}")
@@ -10,25 +14,23 @@ for a in airports[:3]:
    SetSchengen(a)
    PrintAirport(a)
 """
-# Aeroports desde l'arxiu
+#Aeroports desde l'arxiu Airports.txt
 airports = LoadAirports("Airports.txt")
+#Mostrem quants aeroports s'han carregat
 print("Aeroports carregats:", len(airports))
 
-
-# Posar el Schengen
+#Posar el Schengen, recorrem tota la llista.
 for a in airports:
    SetSchengen(a)
 
-
-# Guardar Schengen en un arxiu
+#Guardar Schengen en un arxiu
 resultat = SaveSchengenAirports(airports, "schengen.txt")
 if resultat == 0:
    print("Arxiu schengen.txt creat be.")
 else:
    print("No es pot crear l'arxiu (llista buida).")
 
-
-# Afegir aeroport
+#Afegir aeroport
 nou = Airport("KJFK", 40.6413, -73.7781)
 res_add = AddAirport(airports, nou)
 if res_add == 0:
@@ -36,29 +38,26 @@ if res_add == 0:
 else:
    print(f"Aeroport {nou.code} ja existeix.")
 
-
-# Eliminar aeroport
+#Eliminar aeroport
 res_remove = RemoveAirport(airports, "KJFK")
 if res_remove == 0:
    print("Aeroport KJFK ben eliminat.")
 else:
    print("Aeroport KJFK no trobat per eliminar.")
 
-
-# Mostrar alguns resultats
+#Mostrar alguns resultats
 for a in airports[:3]:  # els 3 primers
    PrintAirport(a)
+print("\nProbar pas 5")
 
-print("\n--- PROBANDO EL PASO 5 ---")
-
-# 1. Probamos el gráfico
-print("Generando gráfico de barras...")
+#Probar el gràfic
+print("S'esta generant el gràfic de barres")
 PlotAirports(airports)
 
-# 2. Probamos el mapa
-print("Generando archivo KML para Google Earth...")
-res_map = MapAirports(airports, "mapa_aeropuertos.kml")
+#Probem el mapa
+print("Generant l'arxiu KML per Google Earth")
+res_map = MapAirports(airports, "mapa_aeroports.kml")
 if res_map == 0:
-    print("¡El archivo 'mapa_aeropuertos.kml' se ha creado bien! Búscalo en tu carpeta.")
+    print("Arxiu mapa aeroports  ben creat, busca'l a la carpeta.")
 else:
-    print("Hubo un error creando el mapa.")
+    print("Error al crear el mapa.")
